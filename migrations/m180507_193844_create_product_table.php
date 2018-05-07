8336c170869e902444e210f5826adf7b52a307e0
denisov <denisov.dmitriy@gmail.com>
2018-05-07 23:50:40 +0300

    product model

---
+++ migrations/m180507_193844_create_product_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `product`.
+ */
+class m180507_193844_create_product_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('product', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'alias' => $this->string()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('product');
+    }
+}
